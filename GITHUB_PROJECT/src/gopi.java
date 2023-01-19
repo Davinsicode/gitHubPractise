553afe5297888cff96d14c0a8ab2670a89d70e05
Gopinath E <Gopinath E@LAPTOP-QME61A2J>
2023-01-19 13:28:51 +0530

    script changes

--- GITHUB_PROJECT/src/gopi.java
+++ GITHUB_PROJECT/src/gopi.java
@@ -1,7 +1,9 @@
+import javax.annotation.processing.AbstractProcessor;
 
 public class gopi {
 
 	public static void main(String[] args) {
+		System.out.println("dcfghkl");
 		// TODO Auto-generated method stub
 
 	}
